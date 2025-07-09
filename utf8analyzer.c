a8b3aa303b6688751747edc0f1c722a68dfa430a
Suh <ehsuh@ieng6-202.ucsd.edu>
2025-07-09 12:34:58 -0700

    made function1

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -10,5 +10,16 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    int is_ascii = 1;  // Assume valid until proven otherwise
+    const char *str = argv[1];
+
+    while (*str) {
+        if ((unsigned char)*str > 127) {
+            is_ascii = 0;
+            break;
+        }
+        str++;
+    }
+
     // implement the UTF-8 analyzer here
 }
