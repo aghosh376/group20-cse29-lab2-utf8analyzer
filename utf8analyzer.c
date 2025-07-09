253502286fad0cdb80a7c06e41e4ef6439c43b43
Ghosh <arg070@ieng6-203.ucsd.edu>
2025-07-09 12:20:50 -0700

    Initialize IO

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -11,4 +11,16 @@ int main(int argc, char *argv[]) {
     }
 
     // implement the UTF-8 analyzer here
+    char string[100]
+    fgets(string, 100, stdin);
+
+    printf("Valid ASCII: %s\n", valid);
+    printf("Uppercased ASCII: %s\n", upper);
+    printf("Length in bytes: %d\n", strlen(string));
+    printf("Number of code points: %d\n", num_codepoints);
+    printf("Code points as decimal numbers: \n", dec_codepoints);
+    printf("Bytes per code point:", bytes_codepoints);
+    printf("Substring of the first 5 code points: %s\n", substring);
+    printf("Animal emojis: \n", emojis);
+    printf("Next character of Codepoint at index 3: %s\n", next_char);
 }
